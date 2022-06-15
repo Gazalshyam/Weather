b53ec821904989d011b1c2b25140cff3383f1918
Gazalshyam <sainramshyam18@gmail.com>
2022-06-15 18:30:29 +0530

    newcommit

--- weather.js
+++ weather.js
@@ -1,28 +1,41 @@
-<script>
-   (function(){
-
-   var body=document.getElementsByTagName("body");
-
-   if (body[0].getAttribute("id")=="collection-54d11b07e4b0a33bbb20a490") {
-     var images = [
-     "http://www.mediafactory.org.au/alexandra-race-lyons/files/2014/07/creative-commons-2-1ef4xzl.png",
-     "http://www.mediafactory.org.au/alexandra-race-lyons/files/2014/07/creative-commons-2-1ef4xzl.png", 
-     "http://www.mediafactory.org.au/alexandra-race-lyons/files/2014/07/creative-commons-2-1ef4xzl.png"
-     ];
-
-     var imgCount = images.length;
-
-     var randNumber=Math.floor((Math.random() * imgCount) + 1);
-
-     imgURL = images[randNumber-1];
-
-     var image=document.getElementsByClassName("thumb-image loaded");
-     image[0].setAttribute("src", imgURL);
-     image[0].setAttribute("data-image", imgURL);
-     image[0].setAttribute("data-src", imgURL);
-     image[0].style.visibility="visible";
-   }
-
- }());
-</script>
+let weather={
+	"apikey":"83936e67d391fbdebd928a7b52e1d653",
+	fetchWeather: function(city){
+		fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+this.apikey)
+			.then((response)=>response.json())
+			.then((data)=>this.displayWeather(data));
+	},
+	displayWeather: function(data){
+		const{name} = data;
+		const{icon,description}=data.weather[0];
+		const{temp,humidity} =data.main;
+		const{speed} = data.wind;
+		console.log(name,icon,description,temp,humidity,speed);
+		document.querySelector(".city").innerText="Weather in "+name;
+		document.querySelector(".icon").innerText="https://openweathermap.org/img/wn/"+icon+".png";
+		document.querySelector(".description").innerText=description;
+		document.querySelector(".temp").innerText=temp+"°С";
+		document.querySelector(".humidity").innerText="Humidity: "+humidity+"%";
+		document.querySelector(".wind").innerText=
+			"Wind Speed: "+speed+"km/h";
+		document.querySelector(".weather").classList.remove("loading");
+		document.body.style.backgroundImage= 
+			"url('https://source.unsplash.com/1600x900/?"+ name +"')";
+	},
+		search: function(){
+			this.fetchWeather(document.querySelector(".search-bar").value);
+		},
+	
+};
+document.querySelector(".search button").addEventListener("click",function(){
+weather.search();
+});
+document
+	.querySelector(".search-bar")
+	.addEventListener("keyup",function(event){
+		if(event.key =="Enter"){
+		weather.search();
+		}
+});
+weather.fetchWeather("Denver");
 
